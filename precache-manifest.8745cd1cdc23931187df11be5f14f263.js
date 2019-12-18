a12be461b37646c7632fa54e37dc0a49af417c1f
Surya Aris <arissurya28@gmail.com>
2019-12-18 17:35:54 +0700

    push

---
+++ precache-manifest.8745cd1cdc23931187df11be5f14f263.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f45f030e07a5178eaf588f9b45e33c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf46358c1b803af31cfd",
+    "url": "/static/css/2.e72b42a1.chunk.css"
+  },
+  {
+    "revision": "f161ecc48c9d68f25fb6",
+    "url": "/static/css/main.6d65ec99.chunk.css"
+  },
+  {
+    "revision": "cf46358c1b803af31cfd",
+    "url": "/static/js/2.a18c0053.chunk.js"
+  },
+  {
+    "revision": "881d757746f3cd1d4a1a68bbfaa7d879",
+    "url": "/static/js/2.a18c0053.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f161ecc48c9d68f25fb6",
+    "url": "/static/js/main.96d41c12.chunk.js"
+  },
+  {
+    "revision": "7e5a09172964795935fb",
+    "url": "/static/js/runtime-main.935ef551.js"
+  },
+  {
+    "revision": "e620c033bc3308609ccdbcfd3dfa4524",
+    "url": "/static/media/Download-Clapperboard-PNG-Picture.e620c033.png"
+  },
+  {
+    "revision": "2a980289fd3b0d7598f42bfafc2e3463",
+    "url": "/static/media/cartplus.2a980289.svg"
+  },
+  {
+    "revision": "02523aa85b21d04a973f378b0e9e0dd1",
+    "url": "/static/media/frozen.02523aa8.jpg"
+  },
+  {
+    "revision": "7100f0d1c9359187bceebd749cfef4d9",
+    "url": "/static/media/jumanji.7100f0d1.jpg"
+  },
+  {
+    "revision": "8f4634075987d2af4d58f591a054f66d",
+    "url": "/static/media/logo.8f463407.png"
+  },
+  {
+    "revision": "cf87a3c8dee0296469009c94550ae988",
+    "url": "/static/media/play.cf87a3c8.png"
+  },
+  {
+    "revision": "1db94683df102e8073c684ece053fc4c",
+    "url": "/static/media/scorpion.1db94683.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
